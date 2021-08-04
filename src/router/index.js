0710c3a94cc942b0af49dc4251c4617d466d3153
MEG HOWE <howem9@gmail.com>
2021-08-04 14:36:43 -0400

    changes about page to welcome page

--- src/router/index.js
+++ src/router/index.js
@@ -18,12 +18,12 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/welcome",
+    name: "Welcome",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import(/* webpackChunkName: "about" */ "../views/Welcome.vue"),
   },
   { path: "/signup", name: "signup", component: Signup },
   { path: "/login", name: "login", component: Login },
