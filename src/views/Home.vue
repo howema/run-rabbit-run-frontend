da57e0640dbb0815e7ed55fb8705f271deccb8f3
MEG HOWE <howem9@gmail.com>
2021-08-16 18:10:29 -0400

    renders favorite thing of current user

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,11 @@
     <h1>Run Rabbit Run</h1>
     <h2><i>Welcome -insert- to your homepage!</i></h2>
     <p>I know your fav thing is -insert- {{}}. Goods news is: this tastes like -insert-.</p>
-    <div v-for="user in users" v-bind:key="user.id">
+    <!-- <div v-for="user in users" v-bind:key="user.id">
       {{ user.favorite_thing }}
-    </div>
+    </div> -->
+    {{ user.favorite_thing }}
+
     <p></p>
     <router-link to="/experiences"><button>View All</button></router-link>
     <p></p>
@@ -26,10 +28,16 @@ export default {
       experiences: [],
       image: {},
       users: [],
+      user: [],
+      current_user_id: localStorage.getItem("user_id"),
     };
   },
   created: function () {
     this.indexUsers();
+    axios.get("/users/" + this.current_user_id).then((response) => {
+      this.user = response.data;
+      console.log(response.data.users);
+    });
   },
   methods: {
     indexUsers: function () {
