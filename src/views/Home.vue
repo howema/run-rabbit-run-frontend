7d541ab4ee31214f35c4f6bcc415b4b3c654eee3
MEG HOWE <howem9@gmail.com>
2021-08-16 16:01:11 -0400

    end of day updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,10 @@
   <div class="home">
     <h1>Run Rabbit Run</h1>
     <h2><i>Welcome -insert- to your homepage!</i></h2>
-    <p>I know your fav thing is -insert-. Goods news is: this tastes like -insert-.</p>
+    <p>I know your fav thing is -insert- {{}}. Goods news is: this tastes like -insert-.</p>
+    <div v-for="user in users" v-bind:key="user.id">
+      {{ user.favorite_thing }}
+    </div>
     <p></p>
     <router-link to="/experiences"><button>View All</button></router-link>
     <p></p>
@@ -13,3 +16,29 @@
     <router-link to="/bucket"><button>View Bucket</button></router-link>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      experiences: [],
+      image: {},
+      users: [],
+    };
+  },
+  created: function () {
+    this.indexUsers();
+  },
+  methods: {
+    indexUsers: function () {
+      axios.get("/users").then((response) => {
+        console.log("users index", response.data);
+        this.users = response.data;
+        console.log(this.users);
+      });
+    },
+  },
+};
+</script>
