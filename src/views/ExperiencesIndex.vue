6840d486ace785467a47a5f5d3e2dabaf87c1450
MEG HOWE <howem9@gmail.com>
2021-08-24 14:16:14 -0400

    card changes

--- src/views/ExperiencesIndex.vue
+++ src/views/ExperiencesIndex.vue
@@ -9,32 +9,55 @@
           </div>
 
           <div class="row">
-            <!-- <div class="columns is-multiline"> -->
             <div v-for="experience in experiences" v-bind:key="experience.id" class="col-3">
-              <div v-if="experience.complete == false">
-                <!-- <div class="card" style="width: 19rem"> -->
-                <router-link v-bind:to="`/experiences/${experience.id}`">
-                  <div class="portfolio-img">
-                    <img v-bind:src="experience.image" class="img-fluid" alt="" />
-                  </div>
-                </router-link>
-
-                <div class="portfolio-info">
-                  <h4>{{ experience.title }}</h4>
-                  <p>{{ experience.notes }}</p>
+              <!-- <div v-if="experience.complete == false"> -->
+              <div class="portfolio-item filter-card">
+                <div class="portfolio-img">
+                  <div class="portfolio-info"></div>
                   <a
                     href="assets/img/portfolio/portfolio-4.jpg"
                     data-gallery="portfolioGallery"
                     class="portfolio-lightbox preview-link"
                     title="Card 2"
                   >
-                    <i class="bx bx-plus"></i>
+                    <!-- <i class="bx bx-plus"></i> -->
                   </a>
+                  <img v-bind:src="experience.image" class="img-fluid" alt="" />
+                  <h4>{{ experience.title }}</h4>
+                  <!-- <p>{{ experience.notes }}</p> -->
+                  <!-- <a href="portfolio-details.html" class="details-link" title="More Details">
+                    <i class="bx bx-link"></i>
+                  </a> -->
+                  <!-- </div> -->
                 </div>
               </div>
             </div>
           </div>
-          <!-- </div> -->
+
+          <!-- <div class="portfolio-item filter-card"> -->
+          <div class="row">
+            <div v-for="experience in experiences" v-bind:key="experience.id" class="col-3">
+              <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+                <div v-if="experience.complete == false">
+                  <router-link v-bind:to="`/experiences/${experience.id}`">
+                    <div class="portfolio-img">
+                      <img v-bind:src="experience.image" class="img-fluid" alt="" />
+                    </div>
+                  </router-link>
+                  <div class="portfolio-info">
+                    <h4>{{ experience.title }}</h4>
+                    <p>{{ experience.notes }}</p>
+                    <a
+                      href="assets/img/portfolio/portfolio-4.jpg"
+                      data-gallery="portfolioGallery"
+                      class="portfolio-lightbox preview-link"
+                      title="Card 2"
+                    ></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
       <router-link to="/experiences/new"><button>Add an Experience</button></router-link>
@@ -45,49 +68,6 @@
 <style>
 /* img {
   max-width: 300px;
-} */
-
-/* * {
-  box-sizing: border-box;
-}
-
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-.column {
-  float: left;
-  width: 25%;
-  padding: 0 10px;
-}
-
-
-.row {
-  margin: 0 -5px;
-}
-
-
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-
-@media screen and (max-width: 600px) {
-  .column {
-    width: 100%;
-    display: block;
-    margin-bottom: 20px;
-  }
-}
-
-
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  padding: 16px;
-  text-align: center;
-  background-color: #f1f1f1;
 } */
 </style>
 
