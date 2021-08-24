bf98e8aaa7a90d846d58cfea9619e4ce8d86ea41
MEG HOWE <howem9@gmail.com>
2021-08-24 15:05:19 -0400

    cards rendering correctly if remove false function

--- src/views/ExperiencesIndex.vue
+++ src/views/ExperiencesIndex.vue
@@ -13,49 +13,21 @@
               <!-- <div v-if="experience.complete == false"> -->
               <div class="portfolio-item filter-card">
                 <div class="portfolio-img">
-                  <div class="portfolio-info"></div>
                   <a
                     href="assets/img/portfolio/portfolio-4.jpg"
                     data-gallery="portfolioGallery"
                     class="portfolio-lightbox preview-link"
                     title="Card 2"
-                  >
-                    <!-- <i class="bx bx-plus"></i> -->
-                  </a>
-                  <img v-bind:src="experience.image" class="img-fluid" alt="" />
-                  <h4>{{ experience.title }}</h4>
-                  <!-- <p>{{ experience.notes }}</p> -->
-                  <!-- <a href="portfolio-details.html" class="details-link" title="More Details">
-                    <i class="bx bx-link"></i>
-                  </a> -->
-                  <!-- </div> -->
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- <div class="portfolio-item filter-card"> -->
-          <div class="row">
-            <div v-for="experience in experiences" v-bind:key="experience.id" class="col-3">
-              <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-                <div v-if="experience.complete == false">
+                  ></a>
                   <router-link v-bind:to="`/experiences/${experience.id}`">
                     <div class="portfolio-img">
                       <img v-bind:src="experience.image" class="img-fluid" alt="" />
                     </div>
+                    <h3>{{ experience.title }}</h3>
                   </router-link>
-                  <div class="portfolio-info">
-                    <h4>{{ experience.title }}</h4>
-                    <p>{{ experience.notes }}</p>
-                    <a
-                      href="assets/img/portfolio/portfolio-4.jpg"
-                      data-gallery="portfolioGallery"
-                      class="portfolio-lightbox preview-link"
-                      title="Card 2"
-                    ></a>
-                  </div>
                 </div>
               </div>
+              <!-- </div> -->
             </div>
           </div>
         </div>
