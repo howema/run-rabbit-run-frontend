bdeb978dc8cc6fabc1ddbeb36bb9a3ca7372c01e
MEG HOWE <howem9@gmail.com>
2021-08-25 18:42:39 -0400

    changes dropdown faq text on bucket page

--- src/views/Bucket.vue
+++ src/views/Bucket.vue
@@ -33,53 +33,53 @@
             </div>
           </section>
         </div>
-        <!-- ======= F.A.Q Section ======= -->
-        <section id="faq" class="faq">
-          <div class="container">
-            <div class="section-title" data-aos="zoom-out">
-              <h2>F.A.Q / What Now?</h2>
-              <p></p>
-            </div>
-
-            <ul class="faq-list">
-              <li>
-                <div data-bs-toggle="collapse" class="collapsed question" href="#faq1">
-                  I did it!
-                  <i class="bi bi-chevron-down icon-show"></i>
-                  <i class="bi bi-chevron-up icon-close"></i>
-                </div>
-                <div id="faq1" class="collapse" data-bs-parent=".faq-list">
-                  <p>The world is your oyster!</p>
-                </div>
-              </li>
-
-              <li>
-                <div data-bs-toggle="collapse" href="#faq2" class="collapsed question">
-                  Enjoy Yourself...
-
-                  <i class="bi bi-chevron-down icon-show"></i>
-                  <i class="bi bi-chevron-up icon-close"></i>
-                </div>
-                <div id="faq2" class="collapse" data-bs-parent=".faq-list">
-                  <p>"It's later than you think."</p>
-                </div>
-              </li>
-
-              <li>
-                <div data-bs-toggle="collapse" href="#faq3" class="collapsed question">
-                  Why is there a button on the "#NothingToIt" page that links to "All"?
-                  <i class="bi bi-chevron-down icon-show"></i>
-                  <i class="bi bi-chevron-up icon-close"></i>
-                </div>
-                <div id="faq3" class="collapse" data-bs-parent=".faq-list">
-                  <p>Cause there's nothing to any of it, baby.</p>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </section>
-        <!-- End F.A.Q Section -->
       </section>
+      <!-- ======= F.A.Q Section ======= -->
+      <section id="faq" class="faq">
+        <div class="container">
+          <div class="section-title" data-aos="zoom-out">
+            <h2>F.A.Q / What Now?</h2>
+            <p></p>
+          </div>
+
+          <ul class="faq-list">
+            <li>
+              <div data-bs-toggle="collapse" class="collapsed question" href="#faq1">
+                I did it! What's next for this lil rabbit?
+                <i class="bi bi-chevron-down icon-show"></i>
+                <i class="bi bi-chevron-up icon-close"></i>
+              </div>
+              <div id="faq1" class="collapse" data-bs-parent=".faq-list">
+                <p>The world is your oyster!</p>
+              </div>
+            </li>
+
+            <li>
+              <div data-bs-toggle="collapse" href="#faq2" class="collapsed question">
+                Why should I enjoy myself?
+
+                <i class="bi bi-chevron-down icon-show"></i>
+                <i class="bi bi-chevron-up icon-close"></i>
+              </div>
+              <div id="faq2" class="collapse" data-bs-parent=".faq-list">
+                <p>"It's later than you think."</p>
+              </div>
+            </li>
+
+            <li>
+              <div data-bs-toggle="collapse" href="#faq3" class="collapsed question">
+                Why is there a button on the "#NothingToIt" page that links to "All"?
+                <i class="bi bi-chevron-down icon-show"></i>
+                <i class="bi bi-chevron-up icon-close"></i>
+              </div>
+              <div id="faq3" class="collapse" data-bs-parent=".faq-list">
+                <p>Cause there's nothing to any of it, baby.</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <!-- End F.A.Q Section -->
     </div>
   </div>
 </template>
